bcdcb8a49d9f359f027c189b76aa92c1010b76d7
KamalAle12 <www.kiranale@gmail.com>
2025-01-25 14:54:54 +0530

    update again

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ import JavaProjects from './components/project/javaProject/JavaProject'
 import GameProjects from './components/project/arProject/gameProject/GameProjects'
 import IotProject from './components/project/iotProject/IotProject'
 import Footer from './components/footer/Footer'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
-   <>
+   <BrowserRouter basename='/portfolio'>
    <Navbar/>
    <Main/>
    <WebProject/>
@@ -20,7 +21,7 @@ const App = () => {
    <GameProjects/>
    <IotProject/>
    <Footer/>
-   </>
+   </BrowserRouter>
   )
 }
 
